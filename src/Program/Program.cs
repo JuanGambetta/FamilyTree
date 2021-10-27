c8ff3cc617345cbd3e58d9d02b6d0b798047045b
JuanGambetta <88690180+JuanGambetta@users.noreply.github.com>
2021-10-27 19:30:07 -0300

    Commit sumaedades

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,6 +7,7 @@ namespace Program
     {
         static void Main(string[] args)
         {
+            /*
             Node n1 = new Node(1);
             Node n2 = new Node(2);
             Node n3 = new Node(3);
@@ -23,8 +24,38 @@ namespace Program
 
             n3.AddChildren(n6);
             n3.AddChildren(n7);
+            */
+            Persona Martin = new Persona("Martin",18);
+            Persona Martina = new Persona("Martina",20);
+            Persona Juan = new Persona("Juan",21);
+            Persona Josefina = new Persona("Josefina",18);
+            Persona Alvaro = new Persona("Alvaro",50);
+            Persona Jose = new Persona("Jose",35);
+            Persona Isabel = new Persona("Isabel",49);
+            Persona Pedro = new Persona("Pedro",21);
 
-            // visitar el árbol aquí
+            Node martin = new Node(Martin);
+            Node martina = new Node(Martina);
+            Node juan = new Node(Juan);
+            Node josefina = new Node(Josefina);
+            Node alvaro = new Node(Alvaro);
+            Node jose = new Node(Jose);
+            Node isabel = new Node(Isabel);
+            Node pedro = new Node(Pedro);
+
+            alvaro.AddChildren(juan);
+            alvaro.AddChildren(pedro);
+            jose.AddChildren(alvaro);
+            isabel.AddChildren(juan);
+            isabel.AddChildren(pedro);
+            martina.AddChildren(isabel);
+            martina.AddChildren(martin);
+
+
+            var sumaEdades = new SumaEdades();
+            alvaro.Accept(sumaEdades);
+            juan.Accept(sumaEdades);
+            
         }
     }
 }
